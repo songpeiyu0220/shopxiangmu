c1e81e6419d91bece32dd7339952f9a60fe56616
Your Name <you@example.com>
2019-04-08 21:35:35 +0800

    登录—-引入表单组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class>
-    <i class="el-icon-edit"></i>
-    <i class="el-icon-share"></i>
-    <i class="el-icon-delete"></i>
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    {{msg}}
+  <div class="login-wrap">
+    <h2>用户登录</h2>
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button class="login-btn" type="primary">登录</el-button>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "login",
   data() {
     return {
-      msg: "sfdfdd"
+      formdata: {
+        username: "",
+        password: ""
+      }
     };
   }
 };
