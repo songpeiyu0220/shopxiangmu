f46a8135024def3364eee66ae3d2f5151d8716a1
Your Name <you@example.com>
2019-04-09 21:04:20 +0800

    home完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,20 +1,139 @@
 <template>
-    <div>home-----
-        {{msg}}
-    </div>
+  <el-container class="container">
+    <el-header>
+      <el-col :span="4">
+        <img src="@/assets/logo.png" alt="图片加载失败">
+      </el-col>
+      <el-col :span="19" class="middle">
+        <h2>电商后台管理系统</h2>
+      </el-col>
+      <el-col :span="1">
+        <div class="grid-content bg-purple">
+          <a href="#" @click.prevent="handleLoginout()" class="loginout">退出</a>
+        </div>
+      </el-col>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu default-active="1" :router="true" :unique-opened="true">
+          <!--     用户管理       -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>用户列表
+            </el-menu-item>
+          </el-submenu>
+          <!--     权限管理       -->
+          <el-submenu index="2" :router="true">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>角色列表
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>权限列表
+            </el-menu-item>
+          </el-submenu>
+          <!--     商品管理       -->
+          <el-submenu index="3" :router="true">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>商品列表
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>分类参数
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>商品分类
+            </el-menu-item>
+          </el-submenu>
+          <!--     订单管理       -->
+          <el-submenu index="4" :router="true">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>订单列表
+            </el-menu-item>
+          </el-submenu>
+          <!--     数据统计       -->
+          <el-submenu index="5" :router="true">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>数据报表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    name:'home',
-    data(){
-        return {
-            msg:'haha'
-        }
+  beforeMount() {
+    console.log("hahahzhixingle");
+    if (!localStorage.getItem("token")) {
+      this.$router.push({
+        name: "login"
+      });
     }
-}
+  },
+  methods: {
+    handleLoginout() {
+      localStorage.clear();
+      this.$router.push({
+        name: "login"
+      });
+      this.$message.warning("退出成功");
+    }
+  }
+};
 </script>
 
 <style>
+.container {
+  background: #b3c0d1;
+  height: 100%;
+}
+.el-header {
+  background-color: b3c0d1;
+  color: #333;
+  /* height: 100%; */
+  /* text-align: center;
+  line-height: 60px; */
+}
+.middle {
+  text-align: center;
+  line-height: 60px;
+}
+h2 {
+  color: white;
+}
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
+.el-aside {
+  background-color: #fff;
+  color: #333;
+}
 
+.el-main {
+  background-color: #e8edf3;
+  color: #333;
+}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,31 +1,69 @@
 <template>
   <div class="login-wrap">
-    <h2>用户登录</h2>
     <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+    <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formdata.password"></el-input>
+        <el-input v-model="formdata.password" type="password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button 
+      @click.prevent="handlelogin()"
+      class="login-btn" type="primary">登录</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+ data() {
     return {
       formdata: {
         username: "",
         password: ""
       }
     };
+  },
+  methods: {
+  async handlelogin() {
+      const res = await this.$http.post(`login`,this.formdata)
+        // console.log(res);
+        const {
+          data: {
+            data,
+            meta:{msg, status}
+          }
+        } = res;
+        if (status === 200) {
+          localStorage.setItem("token", data.token);
+          this.$router.push({
+            name:'home'
+          })
+        } else {
+            this.$message.error(msg);      
+        }
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.login-wrap{
+  height: 100%;
+  background-color: #324152;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form{
+background-color: #fff;
+width: 400px;
+border-radius: 5px;
+padding: 30px;
+}
+.login-btn{
+  width: 100%;  
+}
 </style>
